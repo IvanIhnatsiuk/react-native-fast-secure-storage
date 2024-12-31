28f1d06ecdf9696aa8d9e2480cb6cad17cdc1b93
IHNATSIUK, IVAN <ii3234@exo.att.com>
2024-12-31 12:48:58 +0100

    fix: remove unnecessary import

--- android/src/paper/com/fastsecurestorage/FastSecureStorageModule.kt
+++ android/src/paper/com/fastsecurestorage/FastSecureStorageModule.kt
@@ -5,7 +5,6 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.module.annotations.ReactModule
-import com.fastsecurestorage.FastSecureStorageImpl
 
 @ReactModule(name = FastSecureStorageImpl.NAME)
 class FastSecureStorageModule(
